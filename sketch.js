fba8af8c1bf8873e6145d32bf75e897f6dd2fa6b
VASTRIXCODER <84544398+VASTRIXCODER@users.noreply.github.com>
2021-05-31 11:34:46 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,9 +1,39 @@
-function setup() {
-  createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
-}
 
-function draw() {
-  background(255,255,255);  
-  drawSprites();
+var movingrect,fixedrect;
+function setup()
+{
+  createCanvas(500,500)
+  movingrect=createSprite(200,200,50,50);
+  movingrect.shapeColor="green";
+  movingrect.debug=true;
+  fixedrect=createSprite(300,300,50,30)
+  fixedrect.shapeColor="green";
+  fixedrect.debug=true;
+}
+function draw()
+{
+  background(0);
+  movingrect.x=mouseX;
+  movingrect.y=mouseY;
+
+  if(movingrect.x-fixedrect.x<movingrect.width/2+fixedrect.width/2 && 
+    movingrect.y-fixedrect.y<movingrect.height/2+fixedrect.height/2 &&
+    fixedrect.x-movingrect.x<movingrect.width/2+fixedrect.width/2 &&
+    fixedrect.y-movingrect.y<movingrect.height/2+fixedrect.height/2
+
+    )
+  {
+    movingrect.shapeColor="red";
+    fixedrect.shapeColor="red";
+
+  }
+  else{
+    movingrect.shapeColor="green";
+    fixedrect.shapeColor="green";
+
+  }
+
+
+
+drawSprites();
 }
